bb3e92dd3da50ca1dd30ffd7416027acdbf5a687
Shih-Yu Hwang <shihyuhwang@gmail.com>
2021-07-09 11:35:59 -0500

    add config

--- truffle-config.js
+++ truffle-config.js
@@ -6,12 +6,20 @@ module.exports = {
   contracts_build_directory: path.join(__dirname, "client/src/contracts"),
   networks: {
     development: {
-      port: 8545
-    }
+      host: "127.0.0.1",
+      port: 9545,
+      network_id: "*",
+    },
+    // goerli: {
+    //   provider: function() {
+    //     return new HDWalletProvider(process.env.MNEMONIC, "https://goerli.infura.io/v3/process.env.GOERLI");
+    //   },
+    //   network_id: '5',
+    // },
   },
   compilers: {
     solc: {
       version: "^0.8.6",
-    }
-  }
+    },
+  },
 };
